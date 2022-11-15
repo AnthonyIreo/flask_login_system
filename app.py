8a1c836356f4d5881e595be07933a8f8970e114e
AnthonyIreo <baosheng@buffalo.edu>
2022-11-15 15:42:36 -0500

    11/15

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
+from views import views
+
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/views")
+
+
 
 if __name__ == '__main__':
-    app.run(debug = "True", port=5001)
+    app.run(debug = "True", port=5000)
--- views.py
+++ views.py
@@ -1,3 +1,39 @@
-from flask import Flask
+from flask import Blueprint,render_template,request,jsonify,redirect,url_for
 
+views = Blueprint(__name__, "views")
 
+@views.route("/")
+def home():
+    return render_template('index.html')
+
+@views.route("/<name>/home")
+def homepage(name):
+    string = "Welcome to " + str(name) + "'s homepage."
+    return render_template('index.html', username = string)
+
+# request: ?name=shelly
+@views.route("/home")
+def home_request():
+    # if I request for name
+    name = request.args.get('name')
+    string = "Welcome to " + str(name) + "'s homepage."
+    return render_template('index.html', username = string)
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'Eric'})
+
+# return json
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.get_json"))
+
+# template test
+@views.route("/template")
+def template_test():
+    return render_template('login.html')
