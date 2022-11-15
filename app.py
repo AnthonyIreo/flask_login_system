d1fb7b03ebe245e01190301e66303b554fbc5131
AnthonyIreo <baosheng@buffalo.edu>
2022-11-15 16:46:16 -0500

    11/15

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask,render_template
 from views import views
+from flask_sqlalchemy import SQLAlchemy
+# UserMixin provides the implementation of the properties required by flask_login
+from flask_login import UserMixin
+
+# used to web form
+from flask_wtf import wtforms
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskFrom):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder":"password"})
+    submit = SubmitField("Register")
 
 
 if __name__ == '__main__':
